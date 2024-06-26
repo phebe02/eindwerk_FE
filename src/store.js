2433ddf4c5ac607a2eff71fea554c0eee7552903
phiebs02 <phiebs.d@gmail.com>
2024-06-26 22:32:12 +0200

    react/Redux aanpassing

--- src/store.js
+++
@@ -1,7 +0,0 @@
-// src/store.js
-import { createStore } from "redux";
-import rootReducer from "./reducers"; // Ensure you have an index file in your reducers directory
-
-const store = createStore(rootReducer);
-
-export default store;
