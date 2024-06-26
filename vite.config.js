7a66d165188220d0071fe1fc49dd7340b823dc09
phiebs02 <phiebs.d@gmail.com>
2024-06-26 22:54:42 +0200

    vercel

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,7 @@
-// vite.config.js
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    rollupOptions: {
-      external: ["react", "react-dom", "@reduxjs/toolkit", "react-redux"],
-      output: {
-        globals: {
-          react: "React",
-          "react-dom": "ReactDOM",
-          "@reduxjs/toolkit": "RTK",
-          "react-redux": "ReactRedux",
-        },
-      },
-    },
-  },
 });
